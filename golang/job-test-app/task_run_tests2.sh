8f039b1abf124864f568de8ea4e273585d9733ce
Dwi Ahruman Prayugi <DAHRUMAN@intra.excelcom.co.id>
2018-08-27 11:39:18 +0700

    aaw

---
+++ golang/job-test-app/task_run_tests2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+pwd
+#export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/run-gopher/sample-app/sample-app.d/Godeps/_workspace
+#cd gopath/src/github.com/run-gopher/sample-app/sample-app.d
+
+#go test ./...
